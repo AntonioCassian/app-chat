f6197c819b7750b4a4acb0880ad1536bc1697b67
Antonio Martins <antoniomart849@gmail.com>
2023-10-12 21:42:23 -0300

    feat: project init in node

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import { resolve } from 'path';
+import express from 'express';
+import index from './routes/index';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, 'uploads')));
+  }
+
+  routes() {
+    this.app.use('/', index);
+  }
+}
+
+export default new App().app;
